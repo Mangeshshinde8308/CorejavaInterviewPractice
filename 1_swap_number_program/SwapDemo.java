705351fecbcaedb6ffbc94e99717634ac35d601f
Mangeshshinde8308 <mangeshshinde8308@gmail.com>
2025-06-16 16:55:24 +0530

    initial changes

---
+++ 1_swap_number_program/SwapDemo.java
@@ -0,0 +1,25 @@
+public class SwapDemo{
+    public static void main(String args[]){
+     //methodd 1
+    //  int a=10;
+    //  int b=20;
+    //  System.out.println("before swap a="+a + "  b="+b);
+    //  int tmp;
+    //  tmp=a;
+    //  a=b;
+    //  b=tmp;
+    //  System.out.println("after swap a="+a + "  b="+b);
+
+   // method 2
+   int a=50;
+   int b=60;
+   System.out.println("before swap a="+a + "  b="+b);
+   a=a+b;     //a=10+20=30
+   b=a-b;
+   a=a-b;     //a=30-20=10   
+  
+
+
+
+    }
+}
